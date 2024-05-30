ca265d57ac51c6f71161232e8f27c8043bd19a8a
ikkubaru <iqbal.sp1406@gmail.com>
2024-05-30 12:55:46 +0700

    redesign page pengajuan admin

--- application/views/Admin/pengajuan.php
+++ application/views/Admin/pengajuan.php
@@ -1,24 +1,31 @@
 <h1>Data Pengajuan Lelang</h1>
 <!-- dibuat form jangan lupa -->
-<div class="col-xl-6 col-lg-4 col-md-4 col-sm-6" style="border: 1px solid black;">
+<div class="col-xl-12 col-lg-4 col-md-4 col-sm-6">
     <?php foreach($barang as $thing){ ?>
                            <div class="panel panel-default">
                               <div class="panel-heading bg-info txt-white">
                                     <h4><?= $thing['nama_barang'] ?></h4>
-                                    <h6>Diajukan Oleh : <?= $thing['nama'] ?></h6>
                               </div>
-                              <div class="panel-body">
-                                <img src="<?= base_url('assets/upload/barang/').$thing['foto'] ?>" weight="500px" height="500px">
-                                <h5>Kategori : <?= $thing['id_kategori'] ?></h5> <br>
-                                <h5>Tanggal Diajukan : <?= $thing['tanggal'] ?></h5><br> 
-                                <h5>Deskripsi Barang :</h5> <br>
-                                <h6><?= $thing['deskripsi_barang'] ?></h6><br> 
-                              </div>
-                              <div class="panel-footer">
-                                 <h5>Harga Awal : Rp.<?= number_format($thing['harga_awal'])?></h5>
-                              </div>
-                            </div>
-                            <button type="submit" value="Dibuka">Dibuka</button>
-                            <button type="submit" value="Ditolak">Ditolak</button>
-                            <?php } ?>
+                              <div class="panel-body"> <br>
+                                <img src="<?= base_url('assets/upload/barang/').$thing['foto'] ?>" weight="300px" height="300px">
+                                <div class="col-sm-6 col-xs-12 waves-effect waves-light">
+                                    <form action="<?= base_url('') ?>" method="post">
+                                    <div class="grid-material bg-info"><h5> Rincian Lelang : </h5></div>
+                                    <label for="" class="form-control-label">Diajukan oleh : </label>
+                                        <input type="text" name="" value="<?= $thing['nama'] ?>" class="form-control" readonly>
+                                    <label for="" class="form-control-label">Kategori Lelang : </label>
+                                        <input type="text" name="" value="<?= $thing['id_kategori'] ?>" class="form-control" readonly>
+                                    <label for="" class="form-control-label">Tamggal diajukan : </label>
+                                        <input type="text" name="" value="<?= $thing['tanggal'] ?>" class="form-control" readonly>
+                                    <label for="" class="form-control-label">Deskripsi Barang : </label>
+                                    <p><?= $thing['deskripsi_barang'] ?></p>
+                                            </div>
+                                            <div class="panel-footer">
+                                                <h5>Harga Awal : Rp.<?= number_format($thing['harga_awal'])?></h5>
+                                                <button type="submit" class="btn btn-success" value="dibuka">Disetujui</button>
+                                                <button type="submit" class="btn btn-danger" value="ditolak">Ditolak</button>
+                                                </div>
+                                            </div>
+        <?php } ?>
+                                    </form>
                         </div>
